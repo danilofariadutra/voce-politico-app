64c687f4320abcaa7746442283d147c27c60b3e3
danilo.dutra1444 <danilo.dutra1444@gmail.com>
2022-03-23 17:54:27 -0300

    Splash Screen ok

---
+++ app/src/main/java/com/example/vocepolitico/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.vocepolitico;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+//    private Button start;
+//    start = findViewById(R.id.btn_vamos_la);
+//    start.setBackgroundResource(R.drawable.radius_button);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
