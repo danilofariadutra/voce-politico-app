b8e09d9b09fb5e58325b187a8d6b5c3bf11ac407
danilo.dutra1444 <danilo.dutra1444@gmail.com>
2022-03-24 18:11:41 -0300

    2/3 ViewPager ok Reajustado posições do layout xml.

--- app/src/main/java/com/example/vocepolitico/SplashScreen02.java
+++ app/src/main/java/com/example/vocepolitico/SplashScreen02.java
@@ -1,6 +1,8 @@
 package com.example.vocepolitico;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,5 +13,9 @@ public class SplashScreen02 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(activity_splash_screen_02);
+    }
+
+    public void splash_screen_03(View view) {
+        Toast.makeText(this, "Criar próxima Splash Screen", Toast.LENGTH_SHORT).show();
     }
 }
