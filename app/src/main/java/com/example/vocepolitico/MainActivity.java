4b19e6cdfc6857c425c4fee79911e7bea32e08fe
danilo.dutra1444 <danilo.dutra1444@gmail.com>
2022-03-24 00:05:05 -0300

    1/3 ViewPager ok

--- app/src/main/java/com/example/vocepolitico/MainActivity.java
+++ app/src/main/java/com/example/vocepolitico/MainActivity.java
@@ -2,16 +2,40 @@ package com.example.vocepolitico;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-//    private Button start;
-//    start = findViewById(R.id.btn_vamos_la);
-//    start.setBackgroundResource(R.drawable.radius_button);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    public void splash_screen(View view) {
+        Intent intent = new Intent(MainActivity.this, SplashScreen01.class);
+        startActivity(intent);
+        finish();
     }
 }
--- app/src/main/java/com/example/vocepolitico/SplashScreen01.java
+++ app/src/main/java/com/example/vocepolitico/SplashScreen01.java
@@ -1,4 +1,18 @@
 package com.example.vocepolitico;
 
-public class SplashScreen01 {
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.MotionEventCompat;
+
+import static com.example.vocepolitico.R.layout.activity_splash_screen_01;
+
+public class SplashScreen01 extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(activity_splash_screen_01);
+    }
 }
