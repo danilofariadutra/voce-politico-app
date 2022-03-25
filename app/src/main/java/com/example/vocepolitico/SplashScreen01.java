70f1f6f14da1a4892af449b16d5349b75043390b
danilo.dutra1444 <danilo.dutra1444@gmail.com>
2022-03-25 00:43:17 -0300

    3/3 Splash Screen ok

--- app/src/main/java/com/example/vocepolitico/SplashScreen01.java
+++ app/src/main/java/com/example/vocepolitico/SplashScreen01.java
@@ -3,20 +3,51 @@ package com.example.vocepolitico;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.ActivityOptionsCompat;
 
 import static com.example.vocepolitico.R.layout.activity_splash_screen_01;
 
 public class SplashScreen01 extends AppCompatActivity {
+
+//    ImageButton btnNextSplashScreen;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(activity_splash_screen_01);
+
+//        btnNextSplashScreen = findViewById(R.id.btn_right_splash_screen_01);
+//
+//        btnNextSplashScreen.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//
+//                Animation animation = AnimationUtils.loadAnimation(SplashScreen01.this, R.anim.mixed_anim);
+//                btnNextSplashScreen.startAnimation(animation);
+//
+//                Intent intent = new Intent(SplashScreen01.this, SplashScreen02.class);
+//                ActivityOptionsCompat activityOptionsCompat = ActivityOptionsCompat.makeCustomAnimation(getApplicationContext(), R.anim.move_animation, R.anim.move_animation);
+//                ActivityCompat.startActivity(SplashScreen01.this, intent, activityOptionsCompat.toBundle());
+//                finish();
+//            }
+//        });
     }
 
     public void splash_screen_02(View view) {
         Intent intent = new Intent(SplashScreen01.this, SplashScreen02.class);
+        startActivity(intent);
+        finish();
+    }
+
+    public void main_activity(View view) {
+        Intent intent = new Intent(SplashScreen01.this, MainActivity.class);
         startActivity(intent);
         finish();
     }
--- app/src/main/java/com/example/vocepolitico/SplashScreen03.java
+++ app/src/main/java/com/example/vocepolitico/SplashScreen03.java
@@ -7,23 +7,23 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import static com.example.vocepolitico.R.layout.activity_splash_screen_02;
+import static com.example.vocepolitico.R.layout.activity_splash_screen_03;
 
-public class SplashScreen02 extends AppCompatActivity {
+public class SplashScreen03 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(activity_splash_screen_02);
+        setContentView(activity_splash_screen_03);
     }
 
     public void splash_screen_03(View view) {
-        Intent intent = new Intent(SplashScreen02.this, SplashScreen03.class);
+        Intent intent = new Intent(SplashScreen03.this, MainActivity.class);
         startActivity(intent);
         finish();
     }
 
     public void splash_screen_02(View view) {
-        Intent intent = new Intent(SplashScreen02.this, SplashScreen01.class);
+        Intent intent = new Intent(SplashScreen03.this, SplashScreen02.class);
         startActivity(intent);
         finish();
     }
