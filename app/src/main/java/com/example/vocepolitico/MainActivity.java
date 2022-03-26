e88a5f488a224f4a2ff42960d5e1493c93de4ca8
danilo.dutra1444 <danilo.dutra1444@gmail.com>
2022-03-26 20:23:12 -0300

    Refactoração em MVC

--- app/src/main/java/com/example/vocepolitico/MainActivity.java
+++ app/src/main/java/com/example/vocepolitico/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.vocepolitico;
 
 import android.os.Bundle;
+import android.text.Layout;
 import android.transition.AutoTransition;
 import android.transition.Slide;
 import android.transition.Transition;
@@ -8,40 +9,33 @@ import android.transition.TransitionManager;
 import android.view.Gravity;
 import android.view.View;
 import android.view.animation.AnticipateInterpolator;
-import android.widget.ImageView;
 
-import static com.example.vocepolitico.MainModel.initTransition;
 import static com.example.vocepolitico.R.layout.activity_splash_screen_01;
 
 public class MainActivity extends MainController {
-    ImageView btn_splash_screen_01;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(activity_splash_screen_01);
-        init();
-        initTransition();
-
+        setupAll();
+        initFirstTransition();
     }
 
-    public void splash_01_to_splash_screen_02(View view) {
+    public void splash_01_to_splash_screen_02 (View view) {
         Transition slide = new Slide(Gravity.LEFT);
         slide.setDuration(1750);
 
         TransitionManager.go(scSplashScreen02, slide);
     }
 
-    public void splash_screen_02_to_splash_screen_03(View view) {
-
-
+    public void splash_screen_02_to_splash_screen_03 (View view) {
         Transition slide = new Slide(Gravity.RIGHT);
         slide.setDuration(1750);
 
         TransitionManager.go(scSplashScreen03, slide);
     }
 
-    public void splash_screen_03_to_main(View view) {
+    public void splash_screen_03_to_main (View view) {
         Transition transition;
         transition = new AutoTransition();
         transition.setDuration(2000);
@@ -50,11 +44,20 @@ public class MainActivity extends MainController {
         TransitionManager.go(scMainActivity, transition);
     }
 
-    public void splash_screen(View view) {
+    public void splash_screen (View view){
         Transition transition;
         transition = new AutoTransition();
         transition.setDuration(2500);
         transition.setInterpolator(new AnticipateInterpolator());
-        TransitionManager.go(scSplashScreen02, transition);
+        TransitionManager.go(scSplashScreen01, transition);
+    }
+
+    public void initFirstTransition() {
+        // Cria a transicao para a primeira Splash Screen na onCreate
+        Transition transition;
+        transition = new AutoTransition();
+        transition.setDuration(2000);
+        transition.setInterpolator(new AnticipateInterpolator());
+        TransitionManager.go(scSplashScreen01, transition);
     }
 }
--- app/src/main/java/com/example/vocepolitico/MainModel.java
+++ app/src/main/java/com/example/vocepolitico/MainModel.java
@@ -1,53 +1,10 @@
 package com.example.vocepolitico;
 
 import android.transition.AutoTransition;
-import android.transition.Slide;
 import android.transition.Transition;
 import android.transition.TransitionManager;
-import android.view.Gravity;
-import android.view.View;
 import android.view.animation.AnticipateInterpolator;
 
 public class MainModel extends MainController {
-    public static void initTransition() {
-        // Cria a transicao para a primeira Splash Screen na onCreate
-        Transition transition;
-        transition = new AutoTransition();
-        transition.setDuration(2000);
-        transition.setInterpolator(new AnticipateInterpolator());
-        TransitionManager.go(scSplashScreen01, transition);
-    }
 
-    public static void splash_01_to_splash_screen_02(View view) {
-        Transition slide = new Slide(Gravity.LEFT);
-        slide.setDuration(1750);
-
-        TransitionManager.go(scSplashScreen02, slide);
-    }
-
-    public void splash_screen_02_to_splash_screen_03(View view) {
-
-
-        Transition slide = new Slide(Gravity.RIGHT);
-        slide.setDuration(1750);
-
-        TransitionManager.go(scSplashScreen03, slide);
-    }
-
-    public void splash_screen_03_to_main(View view) {
-        Transition transition;
-        transition = new AutoTransition();
-        transition.setDuration(2000);
-        transition.setInterpolator(new AnticipateInterpolator());
-
-        TransitionManager.go(scMainActivity, transition);
-    }
-
-    public void splash_screen(View view) {
-        Transition transition;
-        transition = new AutoTransition();
-        transition.setDuration(2500);
-        transition.setInterpolator(new AnticipateInterpolator());
-        TransitionManager.go(scSplashScreen02, transition);
-    }
 }
